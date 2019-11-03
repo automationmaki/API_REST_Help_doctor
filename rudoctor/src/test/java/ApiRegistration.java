eefe8e90554138a04c49e7addb966f84c12781fe
Ivan <ivanryshov@mail.ru>
2019-11-03 23:55:25 +0300

    add changes

--- rudoctor/src/test/java/ApiRegistration.java
+++ rudoctor/src/test/java/ApiRegistration.java
@@ -17,22 +17,27 @@ public class ApiRegistration {
         RequestSpecification request = RestAssured.given();
         request.header("Content-Type", "application/json");
         request.body(requestBody.toString());
-        System.out.println(request.body(requestBody.toString()));
         Response response = request.post("http://helpdoctor.tmweb.ru/public/api/registration");
         int statusCode = response.getStatusCode();
         Assert.assertEquals(statusCode, 200);
+        Assert.assertEquals(response.body().asString(), "{\"status\":\"OK\"}");
+        System.out.println("The status code recieved: " + statusCode);
+        System.out.println("Response body: " + response.body().asString());
     }
-    @Test(description = "Sent all parametres")
-    public void sentAll() {
+    @Test(description = "Content-Type xml")
+    public void sentInvalidHeader() {
         RestAssured.config = RestAssured.config().encoderConfig(encoderConfig().appendDefaultContentCharsetToContentTypeIfUndefined(false));
         JSONObject requestBody = new JSONObject();
         requestBody.put("email", RandomStringUtils.randomAlphabetic(5) + "@mail.ru");
         RequestSpecification request = RestAssured.given();
-        request.header("Content-Type", "application/json");
+        request.header("Content-Type", "application/xml");
         request.body(requestBody.toString());
-        System.out.println(request.body(requestBody.toString()));
         Response response = request.post("http://helpdoctor.tmweb.ru/public/api/registration");
         int statusCode = response.getStatusCode();
-        Assert.assertEquals(statusCode, 200);
+        Assert.assertEquals(statusCode, 400);
+        Assert.assertEquals(response.body().asString(), "{\"status\":\"OK\"}");
+        System.out.println("The status code recieved: " + statusCode);
+        System.out.println("Response body: " + response.body().asString());
     }
+
 }
