b99ac42b09937a7c98dfb6c9ae1679525eb8ee1f
Ivan <ivanryshov@mail.ru>
2019-11-05 23:45:26 +0300

    delete

---
+++ rudoctor/src/test/java/DeletingUser.java
@@ -0,0 +1,24 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.json.simple.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DeletingUser {
+    @Test(description = "Sent all parametres")
+    public void sentAllParametres() {
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("email", RandomStringUtils.randomAlphabetic(5) + "@mail.ru");
+        RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
+        request.body(requestBody.toString());
+        Response response = request.delete("http://helpdoctor.tmweb.ru/public/api/registration");
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+        Assert.assertEquals(response.body().asString(), "{\"status\":\"OK\"}");
+        System.out.println("The status code recieved: " + statusCode);
+        System.out.println("Response body: " + response.body().asString());
+    }
+}
